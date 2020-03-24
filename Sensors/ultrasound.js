9b8c4405aa3b42b532f39f848287e44e6a233c86
Erik Bohman <erikboh@student.chalmers.se>
2020-03-24 15:45:31 +0100

    implemented multriple ultrasoundsensors

--- Sensors/ultrasound.js
+++ Sensors/ultrasound.js
@@ -3,21 +3,36 @@ const Gpio = require('pigpio').Gpio;
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECDONDS_PER_CM = 1e6/34321;
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+const trigger1 = new Gpio(23, {mode: Gpio.OUTPUT});
+const echo1 = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
-trigger.digitalWrite(0); // Make sure trigger is low
+const trigger2 = new Gpio(17, {mode: Gpio.OUTPUT});
+const echo2 = new Gpio(27, {mode: Gpio.INPUT, alert: true});
+
+trigger1.digitalWrite(0); // Make sure trigger is low
+trigger2.digitalWrite(0);	
 
 const watchHCSR04 = () => {
-  let startTick;
-
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
+  let startTick1;
+  let startTick2; 	
+	
+  echo1.on('alert', (level1, tick1) => {
+    if (level1 == 1) {
+      startTick1 = tick1;
     } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
+      const endTick1 = tick1;
+      const diff1 = (endTick1 >> 0) - (startTick1 >> 0); // Unsigned 32 bit arithmetic
+      console.log("Ultraljud 1: ", diff1 / 2 / MICROSECDONDS_PER_CM);
+    }
+  });
+
+  echo2.on('alert', (level2, tick2) => {
+    if (level2 == 1) {
+      startTick2 = tick2;
+    } else {
+      const endTick2 = tick2;
+      const diff2 = (endTick2 >> 0) - (startTick2 >> 0); // Unsigned 32 bit arithmetic
+      console.log("Ultraljud 2: ", diff2 / 2 / MICROSECDONDS_PER_CM);
     }
   });
 };
@@ -26,5 +41,6 @@ watchHCSR04();
 
 // Trigger a distance measurement once per second
 setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+  trigger1.trigger(10, 1); // Set trigger high for 10 microseconds
+  trigger2.trigger(10, 1); // Set trigger high for 10 microseconds		
 }, 1000);
