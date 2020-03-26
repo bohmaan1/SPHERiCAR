603f348f064697f1ef5380048e31f3429f083303
Erik Bohman <erikboh@student.chalmers.se>
2020-03-26 15:15:02 +0100

    implemented anti-collision

--- Raspberry/go.js
+++ Raspberry/go.js
@@ -1,6 +1,7 @@
 const { Scanner, Utils, SpheroBolt } = require("spherov2.js");
+const Gpio = require('pigpio').Gpio;
 const gamepad = require("gamepad");
- 
+
 gamepad.init(); 							// Initialize the library
 setInterval(gamepad.processEvents, 16); 	// Create a game loop and poll for events
 setInterval(gamepad.detectDevices, 5000);	// Scan for new gamepads as a slower rate
@@ -13,18 +14,17 @@ const BUTTONS = {
 	CHANGE_VIEW: 6,
 	MENU: 7
 };
- 
 
-const boltNames = ["SB-AE48"/*, "SB-5AEB"*/];
+// Initiera bollarna
+const boltNames = ["SB-5AEB"];
 const numOfBolts = boltNames.length;
+var bolts = Array(numOfBolts);
 
 /*
  * Max speed can be set to anything between 0 - 255.
  * Max speed of the Bolt is 2 m/s which should respond to value 255.
  */
-const maxSpeed = 120;
-
-var bolts = Array(numOfBolts);
+//const maxSpeed = 120;
 
 gamepad.on("down", async function(id, n) {
 	console.log("down", n);
@@ -60,35 +60,68 @@ gamepad.on("down", async function(id, n) {
 	
 });
 
-setInterval(function() {
-	var controller = gamepad.deviceAtIndex(0);
+
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+const MICROSECDONDS_PER_CM = 1e6/34321;
+
+const trigger1 = new Gpio(23, {mode: Gpio.OUTPUT});
+const echo1 = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+const echo2 = new Gpio(27, {mode: Gpio.INPUT, alert: true});
+var distance1 = 0;
+var distance2 = 0;
+
+trigger1.digitalWrite(0); // Make sure trigger is low
+
+const watchHCSR04 = () => {
+  let startTick1;
+  let startTick2; 	
 	
-	for (var i = 0; i < numOfBolts; i++) {
-		if (controller) {
-			// Print entire controller (for debugging)
-			//console.log(controller);
-			
-			var x = controller.axisStates[3*i + 0];
-			var y = controller.axisStates[3*i + 1];
-			
-			/*
-			 * Allow for a dead zone in the center as
-			 * controllers can be giving small values
-			 * when resting (due to bad calibration).
-			 */
-			if (Math.abs(x) < 0.1) x = 0;
-			if (Math.abs(y) < 0.1) y = 0;
-			
-			var speed = Math.trunc(Math.sqrt(x*x+y*y) * maxSpeed);
-			var angle = Math.trunc((Math.atan2(y, x) * 180 / Math.PI + 90 + 360) % 360);
-			
-			// Print (x, y) values (for debugging)
-			//console.log("(" + x + ", " + y + ")");
-			
-			// Send roll command to all bolts
-			//for (var i = 0; i < numOfBolts; i++) if (bolts[i]) bolts[i].roll(speed, angle, []);
-			if (bolts[i]) bolts[i].roll(speed, angle, []);
-		}
-	}
+  echo1.on('alert', (level1, tick1) => {
+    if (level1 == 1) {
+      startTick1 = tick1;
+    } else {
+      const endTick1 = tick1;
+      const diff1 = (endTick1 >> 0) - (startTick1 >> 0); // Unsigned 32 bit arithmetic
+      console.log("Ultraljud 1: ", diff1 / 2 / MICROSECDONDS_PER_CM);
+      distance1 = diff1 / 2 / MICROSECDONDS_PER_CM;
+  if (distance1 < 10 || distance2 < 10) {
+    for (var i = 0; i < numOfBolts; i++) {
+      if (bolts[i]) bolts[i].roll(0, 0, []);
+    }	 		
+  } else {
+    for (var i = 0; i < numOfBolts; i++) {
+      if (bolts[i]) bolts[i].roll(80, 0, []);
+    }		
+  }		
+    }
+  });
+
+  echo2.on('alert', (level2, tick2) => {
+    if (level2 == 1) {
+      startTick2 = tick2;
+    } else {
+      const endTick2 = tick2;
+      const diff2 = (endTick2 >> 0) - (startTick2 >> 0); // Unsigned 32 bit arithmetic
+      console.log("Ultraljud 2: ", diff2 / 2 / MICROSECDONDS_PER_CM);
+      distance2 = diff2 / 2 / MICROSECDONDS_PER_CM;	
+      if (distance1 < 10 || distance2 < 10) {
+    for (var i = 0; i < numOfBolts; i++) {
+      if (bolts[i]) bolts[i].roll(0, 0, []);
+      }	 		
+    } else {
+    for (var i = 0; i < numOfBolts; i++) {
+      if (bolts[i]) bolts[i].roll(80, 0, []);
+      }		
+    }	
+  }
+});
+  	
 	
-}, 50);
+};
+
+watchHCSR04();
+
+// Trigger a distance measurement once per second
+setInterval(() => {
+  trigger1.trigger(10, 1); // Set trigger high for 10 microseconds
+}, 1000);
